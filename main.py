fc6b7bf5683684f4b9cc4cc3ff5ff9e31477f6e5
hermiit <thandobronz@gmail.com>
2018-09-14 12:15:57 -0700

    he

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #// init
 import discord
+import asyncio
 import time
 import requests
 from datetime import datetime, date, time
@@ -7,7 +8,7 @@ import json
 client = discord.Client()
 
 #/ vars
-TOKEN = "lol no"
+TOKEN = ''
 
 dnums = {
     ":one:"  : "Adelhyde",
@@ -37,24 +38,23 @@ julirl = urls['Pages']['julirl']
 dnkurl = urls['Pages']['dnkurl']
 
 #// main
-@client.event
-
-async def on_message(message):
+@client.async_event
+def on_message(message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
 
     if message.content.startswith('**hello'):
         msg = 'Hey, {0.author.mention}.'.format(message)
-        await client.send_message(message.channel, msg)
+        yield from client.send_message(message.channel, msg)
 
     if message.content.startswith('**embtest'):
         embedo=discord.Embed( description="Testing embed description.", color=0x832297)
         embedo.set_author(name="Jill", icon_url=julico)
-        await client.send_message(message.channel, embed=embedo)
+        yield from client.send_message(message.channel, embed=embedo)
 
     if message.content.startswith('**drinktionary'):
-        embedo=discord.Embed(title="Drinktionary", url=dnkurl, description="Here are the ingredients, please react with which number you want to use. (emoji :one:-:five:)", color=0x832297)
+        embedo=discord.Embed(title="Drinktionary", url=dnkurl, description="Here are the ingredients, please post with which number you want to use. (emoji :one:-:five:)", color=0x832297)
         embedo.set_author(name="Jill", url=julirl, icon_url=julico)
         embedo.set_image(url=mixgif)
         embedo.add_field(name=(1), value="Adelhyde", inline=True)
@@ -63,19 +63,14 @@ async def on_message(message):
         embedo.add_field(name=(4), value="Flanergide", inline=True)
         embedo.add_field(name=(5), value="Karmotrine", inline=True)
         embedo.set_footer(icon_url=julico,text='Generated via N1-R V4N-A | ' + datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
-        embmsg = await client.send_message(message.channel, embed=embedo)
-        res = client.wait_for_reaction([':one:',':two:',':three:',':four:',':five:'], user=message.author, timeout=5, message=embmsg)
-        if res:
-            if res.reaction.emoji == ':one:':
-                print('ok man i see you like robbie')
-            elif res['reaction']['emoji'] == ':two:':
-                print('he was number 2')
+        embmsg = yield from client.send_message(message.channel, embed=embedo)
+        res = client.wait_for_message(author=message.author,content='1')
+        yield from client.send_message(message.channel,content=res.content)
 
 
 
-
-@client.event
-async def on_ready():
+@client.async_event
+def on_ready():
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
